dfa8635de605f5ada3e060c8db96c46165017807
배지원(conan) <conan.bae@groundx.xyz>
2018-12-27 12:49:15 +0900

    add struct

---
+++ contracts/QuizKing.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.24;
+
+import "./ERC721/ERC721.sol";
+import "./ERC721/ERC721Enumerable.sol";
+
+contract QuizKing is ERC721, ERC721Enumerable {
+    //event log
+    //struct
+    //function
+}
