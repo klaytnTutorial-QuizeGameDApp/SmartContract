1a510e322263c2677d9b03959c3b26e6bec81c97
배지원(conan) <conan.bae@groundx.xyz>
2019-01-07 19:01:30 +0900

    add func

--- contracts/QuizKing.sol
+++ contracts/QuizKing.sol
@@ -6,7 +6,9 @@ import "./ERC721/ERC721Enumerable.sol";
 contract QuizKing is ERC721, ERC721Enumerable {
     //event log
     event quizCreated();
+
     //struct
+    //quiz에 대한 정보들을 담고있는 struct
     struct QuizData {
         bytes32 quizId;                 // 퀴즈 아이디
         address creator;                // 퀴즈 생성자
@@ -17,12 +19,14 @@ contract QuizKing is ERC721, ERC721Enumerable {
         bool isDone;                    // 문제의 해결 여부
         uint256 prize;                  // 문제 보상 klay
     }
+    
+    // 사용자의 전적을 보여주기 위해 userData struct로 관리
+    struct UserData {
+        uint win;                       // 문제를 풀어서 성공한 횟수
+        uint lose;                      // 문제를 풀어서 실패한 횟수
+    }
 
     mapping( bytes32 => QuizData ) public quizDataSet;
-    struct UserData {
-        uint win;
-        uint lose;
-    }
     mapping( address => UserData ) public record;
 
 
@@ -47,6 +51,18 @@ contract QuizKing is ERC721, ERC721Enumerable {
 
     function payPrize() {
         
+    }
+
+    function getQuiz(bytes32 quizId) public view
+    returns(bytes32, string, bytes32, bool, uint256) {
+        require(quizeDataSet[quizId].quizId != 0, "Quiz Does Not Exist!");
+        return (
+            quizeDataSet[quizId].quizId,
+            quizeDataSet[quizId].quizContent,
+            quizeDataSet[quizId].answerList,
+            quizeDataSet[quizId].isDone,
+            quizeDataSet[quizId].prize
+        );
     }
 
 }
